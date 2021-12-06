acde63ee8d56724a71e431e4b20016cd30df1061
Joshua Lange <9056446+Retroflux@users.noreply.github.com>
2021-12-06 07:51:20 -0500

    Adding finished day 4 part 1 code

--- 2021_AdventOfCode/Python/Day4/day4SolutionCode.py
+++ 2021_AdventOfCode/Python/Day4/day4SolutionCode.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #each row is just a set of five of sequential numbers
 #	ex. row1 = 0->4
 #		row2 = 5->9
@@ -16,3 +17,122 @@
 #>When number is called, search the board strings for the number, identify its position number, and then find its row and col using math for each matching board
 #	>replace the called index with an "X" and check that row and column (ex. row 2 and col 3) for all X's. 
 
+#Can condense the 2x2 array into a 2n list
+	# 0,0 = 0; 0,1 = 1; ... 2,1 = 11
+
+
+	# 1  2  3  4  5
+	# 6  7  8  9  10
+	# 11 12 13 14 15
+	# 16 17 18 19 20
+	# 21 22 23 24 25
+
+from classes.BingoCard import BingoCard
+
+def main():
+
+	listOfBingoCards = list()
+	listOfWinningNumbers = list()
+	firstLineFlag = 0
+	checkRowsForBingo = 0
+	checkColumnsForBingo = 0
+	i = 0
+	j = 0
+	x = 0
+	bingoCardCounter = 0
+	winningBingoCardNumber = 0
+	summationOfRemainingBingoNumbers = 0
+
+	with open("input.txt","r") as fp:
+		for line in fp:
+			if firstLineFlag == 0:
+				listOfWinningNumbers=list(line.strip().split(","))
+				print(listOfWinningNumbers)
+				firstLineFlag += 1
+				next(fp)
+			else:
+				print("Making Bingo Card")
+				print(line)
+				bingoString = str()
+				for i in range(0,5):
+					bingoString += line.strip() + " "
+					try:
+						line = next(fp)
+					except StopIteration as e:
+						pass
+				print(bingoString)
+				listOfBingoCards.append(BingoCard(bingoString,bingoCardCounter))
+				bingoCardCounter += 1
+
+	for bingoCard in listOfBingoCards:
+		bingoCard.BingoCardValues = bingoCard.BingoCardValues.split()
+		print(bingoCard.BingoCardValues)
+
+	(winningBingoCardNumber,winningNumber) = findWinningBingoCard(listOfWinningNumbers,listOfBingoCards)
+	if (winningBingoCardNumber == -1):
+		print("Fatal Error: Ya dun' goofed")
+
+	for number in listOfBingoCards[winningBingoCardNumber].BingoCardValues:
+		print(number)
+		if (number == 'X'):
+			pass
+		else:
+			summationOfRemainingBingoNumbers += int(number.strip())
+	
+	print(f"Summation of the remaining bingo numbers is {summationOfRemainingBingoNumbers}")
+	finalValue = winningNumber * summationOfRemainingBingoNumbers
+	print(f"Final Value: {finalValue}")
+
+
+	#Testing Print
+	# for bingoCard in listOfBingoCards:
+	# 	print("\n\n\n")
+	# 	for i in range(0,5):
+	# 		print(bingoCard.BingoCardValues[5*i:(5*i)+5])
+
+def findWinningBingoCard(listOfWinningNumbers,listOfBingoCards):
+	i = 0
+
+	for winningNumber in listOfWinningNumbers:
+		for bingoCard in listOfBingoCards:
+			for i in range(0,len(bingoCard.BingoCardValues)):
+				if (bingoCard.BingoCardValues[i] == winningNumber):
+					# print(f"Found cell match of {bingoCard.BingoCardValues[i]} and {winningNumber}")
+					bingoCard.BingoCardValues[i] = 'X'
+					checkRowsForBingo = checkRowsForCompletion(bingoCard)
+					checkColumnsForBingo = checkColumnsForCompletion(bingoCard)
+					if(checkRowsForBingo or checkColumnsForBingo):
+						print(f"Winning Bingo Found: Card Number {bingoCard.BingoCardNumber}")
+						print(f"Winning Number:{winningNumber}")
+						return (bingoCard.BingoCardNumber,int(winningNumber))
+	return -1
+
+def checkRowsForCompletion(bingoCard):
+	i = 0 
+	j = 0
+
+	for i in range(0,25,5):
+		counter = 0
+		for j in range(i,i+5):
+			if bingoCard.BingoCardValues[j] == 'X':
+				counter += 1
+		if counter == 5:
+			return True
+	return False	
+	
+def checkColumnsForCompletion(bingoCard):
+	#y=5x+i
+
+	for i in range(0,5):
+		counter = 0
+		for j in range(0,5):
+			if(bingoCard.BingoCardValues[(5*j)+i] == 'X'):
+				counter +=1
+		if counter == 5:
+			return True			
+	return False	
+
+
+
+if __name__ == '__main__':
+	main()
