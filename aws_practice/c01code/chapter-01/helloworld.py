378855c888227f8b747e95d149ae27adafeea0bc
Joshua Lange <jlange@uoguelph.ca>
2022-04-25 10:45:39 -0400

    Completing Exercise 1-5 with some additional testing. MP3 files created uploaded as well.

--- aws_practice/c01code/chapter-01/helloworld.py
+++ aws_practice/c01code/chapter-01/helloworld.py
@@ -7,11 +7,17 @@ import boto3
 polly = boto3.client('polly')
 
 # Call the Amazon Polly Web Service
-result = polly.synthesize_speech(Text='Hello World!',
+result1 = polly.synthesize_speech(Text='Hello World!',
                                  OutputFormat='mp3',
-                                 VoiceId='Aditi')
+                                 VoiceId='Matthew')
+result2 = polly.synthesize_speech(Text='Hello Virtual World!',
+                                 OutputFormat='mp3',
+                                 VoiceId='Kendra')
 
 # Save the Audio File
-audio = result['AudioStream'].read()
+audio = result1['AudioStream'].read()
 with open("helloworld.mp3","wb") as file:
+    file.write(audio)
+audio = result2['AudioStream'].read()
+with open("hellovirtualworld.mp3","wb") as file:
     file.write(audio)
