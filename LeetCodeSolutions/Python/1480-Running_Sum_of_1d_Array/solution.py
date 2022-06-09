7ebb0b3acaeba8b4f04f8cac7ed327120c810232
Joshua Lange <jlange@uoguelph.ca>
2022-06-09 14:24:26 -0400

    1480 Completed in C - Updating README.md and adding new solution

--- LeetCodeSolutions/Python/1480-Running_Sum_of_1d_Array/solution.py
+++ LeetCodeSolutions/Python/1480-Running_Sum_of_1d_Array/solution.py
@@ -1,3 +1,10 @@
+# https://leetcode.com/submissions/detail/718302600/
+# Date of Submission: 2022-06-09
+
+# Runtime: 85 ms, faster than 98.93% of Python online submissions for Median of Two Sorted Arrays.
+# Memory Usage: 14.3 MB, less than 24.75% of Python3 online submissions for Median of Two Sorted Arrays.
+
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         outputList = []
