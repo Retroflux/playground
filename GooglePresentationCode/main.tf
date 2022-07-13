9627b16f3a01e59e2d34ddbf07b4a633ba49c196
Joshua Lange <jlange@uoguelph.ca>
2022-07-13 12:36:29 -0400

    bugfixes for client needed, attempting to get server spinning and accessible

--- GooglePresentationCode/main.tf
+++ GooglePresentationCode/main.tf
@@ -14,27 +14,8 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "app_client" {
-  ami           = "ami-071e6cafc48327ca2" # Debian 11
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "2022-07-18_GoogleCodeExample",
-    AMI = "Debian11",
-    Resource_Type = "EC2"
-  }
-}
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-071e6cafc48327ca2"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "2022-07-18_GoogleCodeExample",
-    AMI = "Debian11",
-    Resource_Type = "EC2"
-  }
-}
 
 #internet gateway
 resource "aws_internet_gateway" "app_gateway" {
@@ -54,6 +35,7 @@ resource "aws_vpc" "client-server-VPC" {
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.client-server-VPC.id #grabs VPC above
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "2022-07-18_GoogleCodeExample",
@@ -61,20 +43,6 @@ resource "aws_subnet" "subnet1" {
   }
 }
 
-#EIPs for server and client
-resource "aws_eip" "single-elastic-IP" {
-  instance = aws_instance.app_client
-  vpc      = true
-}
-
-resource "aws_eip" "single-elastic-IP" {
-  instance = aws_instance.app_server
-  vpc      = true
-
-  tags = {
-    Name = "2022-07-18_GoogleCodeExample"
-  }
-}
 
 #non-default route table
 resource "aws_route_table" "route-table" {
@@ -128,14 +96,14 @@ resource "aws_security_group" "open-port-12052" {
 #    cidr_blocks      = [aws_vpc.client-server-VPC] #TODO switch to VPC once it's working
     cidr_blocks      = ["0.0.0.0/0"] #TODO switch to VPC once it's working
   }
-#    ingress {
-#    description      = "TLS from VPC"
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "tcp"
-##    cidr_blocks      = [aws_vpc.client-server-VPC] #TODO switch to VPC once it's working
-#    cidr_blocks      = ["0.0.0.0/0"] #TODO switch to VPC once it's working
-#  }
+    ingress {
+    description      = "SocketProtocol"
+    from_port        = 12052
+    to_port          = 12052
+    protocol         = "tcp"
+#    cidr_blocks      = [aws_vpc.client-server-VPC] #TODO switch to VPC once it's working
+    cidr_blocks      = [aws_vpc.client-server-VPC.cidr_block] #TODO switch to VPC once it's working
+  }
   # TODO lock down after initial testing
   egress {
     from_port        = 0
@@ -146,6 +114,90 @@ resource "aws_security_group" "open-port-12052" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_12052"
   }
+}
+
+resource "aws_network_interface" "webserver_nic" {
+  subnet_id = aws_subnet.subnet1.id
+  private_ips = ["10.0.1.50"]
+  security_groups = [aws_security_group.open-port-12052.id]
+}
+
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.webserver_nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.app_gateway]
+}
+
+#resource "aws_instance" "app_client" {
+#  ami           = "ami-071e6cafc48327ca2" # Debian 11
+#  instance_type = "t2.micro"
+#  availability_zone = "us-west-2a"
+#  key_name = "main-key"
+#
+#  network_interface {
+#    device_index         = 0
+#    network_interface_id = aws_network_interface.webserver_nic.id
+#  }
+#  tags = {
+#    Name = "2022-07-18_GoogleCodeExample",
+#    AMI = "Debian11",
+#    Resource_Type = "EC2"
+#  }
+#  user_data = <<EOF
+#                #!/bin/bash
+#                sudo apt update
+#                sudo apt upgrade -y
+#                sudo apt install git -y
+#                sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
+#                wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+#                tar -xf Python-3.9.1.tgz
+#                cd Python-3.9.1
+#                ./configure --enable-optimizations
+#                make -j 2
+#                sudo make altinstall
+#                python3.9 --version
+#                git clone https://github.com/Retroflux/playground/tree/main/GooglePresentationCode
+#                make > output.txt
+#                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+#                ./server 12052
+#                EOF
+#}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-071e6cafc48327ca2"
+  instance_type = "t2.micro"
+  availability_zone = "us-west-2a"
+  key_name = "main-key"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.webserver_nic.id
+  }
+
+  tags = {
+    Name = "2022-07-18_GoogleCodeExample",
+    AMI = "Debian11",
+    Resource_Type = "EC2"
+  }
+  user_data = <<EOF
+                #!/bin/bash
+                sudo apt update
+                sudo apt upgrade -y
+                sudo apt install git -y
+                sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
+                wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+                tar -xf Python-3.9.1.tgz
+                cd Python-3.9.1
+                ./configure --enable-optimizations
+                make -j 2
+                sudo make altinstall
+                python3.9 --version
+                git clone https://github.com/Retroflux/playground/tree/main/GooglePresentationCode
+                make > output.txt
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                ./server 12052
+                EOF
 }
