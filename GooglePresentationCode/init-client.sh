664a56a7aaa43728545d7f76e5d3a7c0ead629cd
Joshua Lange <jlange@uoguelph.ca>
2022-07-14 16:18:32 -0400

    Added third common security group for 443, 22, and 80, then the ingress and egress for the server "diode"

--- GooglePresentationCode/init-client.sh
+++ GooglePresentationCode/init-client.sh
@@ -5,12 +5,10 @@ REPOSITORY="https://github.com/Retroflux/playground/"
 apt update
 apt upgrade -y
 apt install git -y
-apt install apache2 -y
 apt install make -y
 apt install gcc -y
 mkdir clientCode
 git clone $REPOSITORY ./clientCode
-systemctl start apache2
-bash -c 'echo your very first web server > /var/www/html/index.html'
-make -C clientCode/GooglePresentationCode &> makeErrorOutput.txt
+make -C clientCode/GooglePresentationCode &> makeOutput.txt
+chmod u+x clientCode
 reboot
--- GooglePresentationCode/init-server.sh
+++ GooglePresentationCode/init-server.sh
@@ -5,12 +5,10 @@ REPOSITORY="https://github.com/Retroflux/playground/"
 apt update
 apt upgrade -y
 apt install git -y
-apt install apache2 -y
 apt install make -y
 apt install gcc -y
 mkdir serverCode
 git clone $REPOSITORY ./serverCode
-systemctl start apache2
-bash -c 'echo your very first web server > /var/www/html/index.html'
 make -C serverCode/GooglePresentationCode &> makeOutput.txt
+chmod u+x serverCode
 reboot
