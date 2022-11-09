e3e1c4cc0a7685f5e6cb36e5458d40a8a5afe0df
Retroflux <9056446+Retroflux@users.noreply.github.com>
2022-11-09 13:56:45 -0500

    Adding println practice file

---
+++ RustPractice/syntax_practice/src/print_to_console.rs
@@ -0,0 +1,8 @@
+pub fn run(){
+    //Print to console
+    println!("Hello from the print_to_console.rs file!");
+    println!("format {} arguments", "some");
+    println!("The world is {} and humanity is {}", "big", "small");
+    println!("1, 2, {0}, 4, {1}, {0}, 7, 8, {0}, {1}, 11, {0}, 13, 14, {0}{1}", "fizz", "buzz");
+
+}
