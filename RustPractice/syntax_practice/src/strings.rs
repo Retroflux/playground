1b27de97cac9dfcfd0d9febc2bb74b1b2c27f2aa
Joshua Lange <9056446+Retroflux@users.noreply.github.com>
2022-11-09 20:31:46 -0500

    Adding strings file with some tests

--- RustPractice/syntax_practice/src/strings.rs
+++ RustPractice/syntax_practice/src/strings.rs
@@ -1,6 +1,35 @@
+/*There are two types of strings:
+
+str = primitive string, fixed in length, immutable
+String = growable, heap-allocated data structure; used when you need to modify or own data
+
+*/
+
 pub fn run(){
 
    println!("Strings begin:");
+
+   let mut growable_string = String::from("Hello");
+
+   //Get length
+
+   println!("length of growable_string: {}", growable_string.len());
+
+
+   // Push only char variables
+   growable_string.push('!');
+
+   println!("{}", growable_string);
+
+   //Push string of characters
+   growable_string.push_str(" You stinky guy");
+
+   println!("{}", growable_string);
+   println!("length of growable_string: {}", growable_string.len());
+
+   let is_the_string_empty = growable_string.is_empty();
+
+   println!("String is empty: {:?}", is_the_string_empty);
 
 
 }
