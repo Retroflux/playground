07a77b1f60ece6e2051e18240f583420c7aeec54
Retroflux <retroflux.business@gmail.com>
2022-11-13 10:10:19 -0500

    added for loops to the loops.rs

--- RustPractice/syntax_practice/src/loops.rs
+++ RustPractice/syntax_practice/src/loops.rs
@@ -22,24 +22,34 @@ pub fn run(){
    }
 
    //reverse code as the infinite loop, but with a known exit value and printing one value per line beside the value of count 
+   //commented out the print lines to avoid too much output
    while count > 0 {
 
-      print!("Count = {count}, fizzbuzz value = ");
-      count-=1;
-      if count % 15 == 0{
-         println!("fizzbuzz ");
-      }else if count % 3 == 0{
-         println!("fizz ");
-      }else if count % 5 == 0{
-         println!("buzz ");
-      }else{
-         println!("{count} ");
-      }
+      // print!("Count = {count}, fizzbuzz value = ");
+      count -= 1;
+      // if count % 15 == 0{
+         // println!("fizzbuzz ");
+      // }else if count % 3 == 0{
+         // println!("fizz ");
+      // }else if count % 5 == 0{
+         // println!("buzz ");
+      // }else{
+         // println!("{count} ");
+      // }
+   }
+   println!("\n\n");
+   // Same as first batch of code, just counting to 20 with the for loop variable instead of count. Much more compact code!
+   for i in 1..20{
 
-      if count >=101{
-         break;
+      if i % 15 == 0{
+         print!("fizzbuzz ");
+      }else if i % 3 == 0{
+         print!("fizz ");
+      }else if i % 5 == 0{
+         print!("buzz ");
+      }else{
+         print!("{i} ");
       }
    }
-
 
 }
