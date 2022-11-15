b73653980d5fe4f25c5edcf2c91260ca7aa24027
Retroflux <retroflux.business@gmail.com>
2022-11-14 22:32:49 -0500

    Client-server communication built. Next is file I/O and storage on 'server'.

--- RustPractice/ClientServerProject/RustEquivalent/src/server.rs
+++ RustPractice/ClientServerProject/RustEquivalent/src/server.rs
@@ -1,26 +1,46 @@
-use std::net;
+use std::io::Read;
 use std::env;
+use std::net::Shutdown;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-const DEFAULTTHREADSIZE: u16 = 2048;
+// const DEFAULTTHREADSIZE: u16 = 2048;
 static DEFAULTPORTNUM: u16 = 2048;
 
-struct ThreadArgs{
-    worker_id : u32,
-    thread_val : u32
-}
+// struct ThreadArgs{
+//     worker_id : u32,
+//     thread_val : u32
+// }
 
-struct FileInformation{
-    total_file_size : u128,
-    total_received : u128,
-    worker_id : u32,
-    name_of_file : String
-}
+// struct FileInformation{
+//     total_file_size : u128,
+//     total_received : u128,
+//     worker_id : u32,
+//     name_of_file : String
+// }
 
 
-fn handle_client(stream: TcpStream){
-    
+fn handle_client(stream: &mut TcpStream){
+
+    let mut buffer = [0;10];
+    println!("{:?}",stream.peer_addr());
+    println!("Peeked content: {}", stream.peek(&mut buffer).expect("peek failed"));
+    loop{
+        let read_value = stream.read(&mut buffer);
+        if let Err(_) = &read_value{
+            println!("ERROR");
+            break;
+        }        
+        if let Ok(0) = &read_value{
+            println!("EOF Reached");
+            break;
+        }
+        if let Ok(_) = &read_value{
+            let s = std::str::from_utf8(&buffer).unwrap();
+            println!("Buffer: {:?}", s);
+        }
+    }
+    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
 }
 
 
@@ -41,7 +61,7 @@ fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(receiving_address)?;
 
     for stream in listener.incoming() {
-        handle_client(stream?);
+        handle_client(&mut stream?);
     }
 
     Ok(())
