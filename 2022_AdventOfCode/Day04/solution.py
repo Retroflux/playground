a5423e8a821a1b80a160b053de068d0d50440fa6
Retroflux <retroflux.business@gmail.com>
2022-12-04 18:47:48 -0500

    completed day 4 after some head banging

--- 2022_AdventOfCode/Day04/solution.py
+++ 2022_AdventOfCode/Day04/solution.py
@@ -2,24 +2,39 @@ def main():
 
    filePtr = open("input.txt", "r")
 
-   rollingCounter = 0
+   rollingCounter = 0 #part 1 counter
+   secondCounter = 0 #part 2 counter
 
    for line in filePtr:
-      line=line.strip()
       
+      #format and split input
+      line=line.strip()
       leftSide = line.split(",")[0] #ex format: 1-2
       rightSide = line.split(",")[1] #ex format: 1-2
 
-      if leftSide == rightSide:
-         continue
+      #typecast to int
+      left_min = int(leftSide.split("-")[0]) 
+      left_max = int(leftSide.split("-")[1])
+      right_min = int(rightSide.split("-")[0]) 
+      right_max = int(rightSide.split("-")[1])
 
-      if (leftSide.split("-")[0] >= rightSide.split("-")[0]): #left MAY be within right side
-         if(leftSide.split("-")[1] <= rightSide.split("-")[1]): #left IS inside right side
-            rollingCounter += 1
-      elif(rightSide.split("-")[0] >= leftSide.split("-")[0]): #right MAY be within left side
-         if(rightSide.split("-")[1] <= leftSide.split("-")[1]): #right IS inside left side
-            rollingCounter += 1
+
+      #part 1
+      if(left_min <= right_min and left_max >= right_max):
+         rollingCounter+=1
+      elif(right_min <= left_min and right_max >= left_max):
+         rollingCounter+=1
+
+      #part 2 - use negations to confirm no overlap
+      if(left_min > right_max or right_min > left_max):
+         continue
+      elif(right_max < left_min or left_max < right_min):
+         continue
+      else:
+         secondCounter+=1
+
    print(rollingCounter)
+   print(secondCounter)
 
    return
 
