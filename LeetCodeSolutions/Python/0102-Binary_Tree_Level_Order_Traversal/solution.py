2c9ede01cf2b9573e96298eb3af4ad5e322d747a
Retroflux <9056446+Retroflux@users.noreply.github.com>
2023-03-16 10:01:37 -0400

    Adding 1572 and 0566, updating README

--- LeetCodeSolutions/Python/0102-Binary_Tree_Level_Order_Traversal/solution.py
+++ LeetCodeSolutions/Python/0102-Binary_Tree_Level_Order_Traversal/solution.py
@@ -6,7 +6,7 @@
 
 # Problem:
 #  Given the root of a binary tree, return the level order traversal of its 
-#  nodes' values. (i.e., from left to right, level by level).
+#   nodes' values. (i.e., from left to right, level by level).
 
 # Definition for a binary tree node.
 # class TreeNode:
